bc8ca9cd6d0ef826f1950d2951827514f04721f3
Yash Pal <pyash3137@gmail.com>
2025-06-18 18:07:46 +0530

    added protected routes

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -2,18 +2,19 @@ import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
 import { useUser } from '../../hooks/useUser'
+import GuestOnly from '../../components/auth/GuestOnly'
 
 const AuthLayout = () => {
   const {user} = useUser()
   console.log(user)
   return (
-   <>
+   <GuestOnly>
    <StatusBar style="auto" />
    <Stack 
    screenOptions={{headerShown:false,animation:"none"}}>
 
    </Stack>
-   </>
+   </GuestOnly>
   )
 }
 
---
+++ components/auth/GuestOnly.jsx
@@ -0,0 +1,24 @@
+import { useRouter } from 'expo-router';
+import { useUser } from '../../hooks/useUser';
+import { useEffect } from 'react';
+import { Text } from 'react-native';
+
+const GuestOnly =({ children}) =>{
+    const {user , authChecked} = useUser()
+    const router = useRouter()
+
+    useEffect(()=>{
+        if(authChecked && user !== null){
+            router.replace('/profile')
+        }
+
+    },[user,authChecked])
+    if(!authChecked || user){
+        return (
+            <Text>Loading...</Text>
+        )
+    }
+    return children
+}
+
+export default  GuestOnly
